2f472a6a5d8437a3378988eb02b1a5e6a1ad47af
Ganga <ganga99@zoho.com>
2025-09-18 23:47:24 -0500

    cart page and test code is updated after Naveen's review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,11 +1,19 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
-	String cart = "cart";
+//	String cart = "cart";
+	By cart = By.id("cart");
 	
 	public void addToCart() {
 		System.out.println("adding to cart");
+		System.out.println("adding to cart");
+		System.out.println("adding to cart");
+		System.out.println("adding to cart");
+		System.out.println("adding to cart");
+		
 	}
 
 }
