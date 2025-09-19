a3147c74dc2c74f283e51ae679baa9ab179a9cf8
Ganga <ganga99@zoho.com>
2025-09-18 23:09:05 -0500

    added cart test

---
+++ src/test/java/com/qa/opencart/tests/CartPageTest.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class CartPageTest {
+	
+	@Test
+	public void cartTest() {
+		System.out.println("cart testing");
+	}
+
+}
